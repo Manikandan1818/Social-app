da8de9a693894e33dd46f77eb65e633162fe060e
Manikandan1818 <simbumanii18@gmail.com>
2022-07-20 23:03:15 +0530

    like funcationlity updated!

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { MoreVert } from "@material-ui/icons";
 import "./post.css";
 import { Users } from "../../dummyData";
 
 export const Post = ({ post }) => {
   // console.log(post);
+
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
 
   return (
     <div className="post">
@@ -30,9 +39,19 @@ export const Post = ({ post }) => {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="/assets/like.png" alt="" />
-            <img className="likeIcon" src="/assets/heart.png" alt="" />
-            <span className="postLikeCounter">{post.like} people like it</span>
+            <img
+              className="likeIcon"
+              src="/assets/like.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src="/assets/heart.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
